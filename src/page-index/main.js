f2275ba29d2fd9dffe51f228e260da2ae1922120
Akansh Gulati <akanshgulati2009@gmail.com>
2020-04-11 20:13:22 +0530

    fix(update): links

--- src/page-index/main.js
+++ src/page-index/main.js
@@ -2,4 +2,10 @@ require('./page.css')
 require('./small-page.css')
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded', 'page-index')
-});
+})
+window.chromeOpen = () => {
+  window.location.href = 'https://chrome.google.com/webstore/detail/covid-19-tracker/ehpkefnpbbmohfkanokkjklihkjbamjm'
+}
+window.firefoxOpen = () => {
+  window.location.href = 'https://addons.mozilla.org/en-US/firefox/addon/covid-19-tracker'
+}
